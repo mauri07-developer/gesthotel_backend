b01421649095ca411ea8451034554d811b8dd9e1
Mauricio Custodio <mauricioramirocustodiozune@gmail.com>
2024-07-09 13:27:43 -0500

    [ADD] ruta api para el piso

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FloorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Ruta Pisos
+Route::get('/floors', [FloorController::class, 'index']);
+
+Route::get('/floor/{id}',function(){
+    return "Obtener un piso";
+});
+
+Route::post("/floors",function(){
+    return "Registrar un piso";
+});
+
+Route::put("/floors/{id}",function(){
+    return "Actualizar piso";
+});
+
+Route::delete("/floors/{id}",function(){
+    return "Eliminar piso";
 });
