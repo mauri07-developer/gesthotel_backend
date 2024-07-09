0c1a6638a33697faa9d2e867824f761ade5cc0df
Mauricio Custodio <mauricioramirocustodiozune@gmail.com>
2024-07-09 13:33:25 -0500

    [ADD] controlador, modelos, migrations, correspondientes a un 15%

---
+++ app/Http/Controllers/FloorController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Floor;
+use Illuminate\Http\Request;
+
+class FloorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {   
+        $floors = Floor::all();
+
+        // if ($floors->isEmpty()) {
+        //    $data= 
+        //    [
+        //     'message' => "No se encontraron pisos registrados",
+        //     'status'=>404
+        //    ];
+        //    return response()->json($data,404);
+        // }
+
+        $data= 
+        [
+         'floors' => $floors,
+         'status'=>200
+        ];
+
+        return response()->json($data,200);    
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Floor $floor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Floor $floor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Floor $floor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Floor $floor)
+    {
+        //
+    }
+}
