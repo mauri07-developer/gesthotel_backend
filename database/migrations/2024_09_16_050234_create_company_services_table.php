332745ab8ef9781f5624a0b27af6e37e81db1ee5
Gloria Noelia <gloro@unprg.edu.pe>
2024-09-16 20:05:26 -0500

    Migracion Company_service y dayli_box

--- database/migrations/2024_09_16_050234_create_company_services_table.php
+++ database/migrations/2024_09_16_050234_create_company_services_table.php
@@ -12,7 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('company_services', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id_company_service');
+            $table->unsignedInteger('id_company');
+            $table->foreign('id_company')->references('id_company')->on('companies');
+            $table->unsignedBigInteger('id_service');
+            $table->foreign('id_service')->references('id_service')->on('services');
+            $table->string('comment')->nullable();
+            $table->decimal('price',10,2);
+            $table->integer('state')->default(1)->comment('1->activo 2 ->inactivo');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/migrations/2024_09_16_050344_create_daily_boxes_table.php
+++ database/migrations/2024_09_16_050344_create_daily_boxes_table.php
@@ -12,8 +12,25 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('daily_boxes', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id_daily_box');
+            $table->unsignedBigInteger('id_box');
+            $table->foreign('id_box')->references('id_box')->on('boxes');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id_user')->on('users');
+            $table->timestamp('date_initial');
+            $table->timestamp('date_final')->nullable();
+            $table->decimal('initial',10,2)->comment('Monto con que se apertura caja');
+            $table->decimal('final',10,2);
+            $table->decimal('income',10,2);
+            $table->decimal('expense',10,2);
+            $table->decimal('deposit',10,2);
+            $table->decimal('retirement',10,2);
+            $table->decimal('card',10,2);
+            $table->decimal('transfer',10,2);
+            $table->json('details')->comment('registro de todos ingresos y egresos');
+            $table->integer('state')->default(1)->comment('1->creada , 2 -> cerrada');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
